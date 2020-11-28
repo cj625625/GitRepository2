902feb9fc247b92304620d9973782328d5bc80b7
caijin <15850207397@163.com>
2020-11-28 20:22:43 +0800

    在Person类中添加了age属性。

---
+++ TestModule01/src/com/caijin/com/Person.java
@@ -0,0 +1,11 @@
+package com.caijin.com;
+
+/**
+ * @Auther : cai_jin
+ * @Date : 2020-11-28 20:20
+ * @Description : com.caijin.com
+ * @Version: 1.0
+ */
+public class Person {
+    private int age;
+}
